dfc05224340d474961fbcc16832172c7d37c5316
toyiny <yusuf.f@husky.neu.edu>
2020-04-02 21:45:29 -0400

    Adding example and chest xray images

---
+++ correlations_example/submit.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=spark_multinode
+#SBATCH --ntasks=112                  #will request 112 cores across multiple nodes
+#SBATCH --time=01:00:00             #time to run the job
+#SBATCH -p short                             #short partition with a 24hour time limit
+#SBATCH --constraint=ib               #run only on the new nodes with IB network
+#SBATCH --mem=0                         #get all available memory on the node
+
+module load spark/2.3.2-hadoop2.7
+
+# This syntax tells spark to use all cpu cores on the node.
+export MASTER="local[*]"
+
+script=/shared/centos7/spark/2.3.2-hadoop2.7/examples/src/main/python/mllib/correlations.py
+data=/shared/centos7/spark/2.3.2-hadoop2.7/data/mllib/sample_linear_regression_data.txt
+
+        # check what time it is before the job starts
+        STARTT=`date +%s`
+
+        spark-submit --master $MASTER --total-executor-cores $SLURM_NTASKS $script $data
+
+        # check what time it is when the command finishes
+        ENDT=`date +%s`
+        ELAPSED=`echo 0 | awk -v e=$ENDT -v s=$STARTT '{print e-s}'`
+                     echo "Number of cores: $SLURM_NTASKS , run-time: $ELAPSED seconds" >> performance.log
